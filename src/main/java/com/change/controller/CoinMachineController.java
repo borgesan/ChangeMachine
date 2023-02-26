e1c4e07fcead48fb2b6222ef0ed3662d4eac9bba
Andre Borges <andredesouza.borges@gmail.com>
2023-02-26 12:48:19 -0500

    Some changes

--- src/main/java/com/change/controller/CoinMachineController.java
+++ src/main/java/com/change/controller/CoinMachineController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/com/change/service/CoinMachineService.java
@@ -0,0 +1,5 @@
+package com.change.service;
+
+public class CoinMachineService {
+
+}
